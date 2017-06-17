1095c18580a939ee50a5cb69810b24a191db731c
rvignesh <rvignesh@ADFL-1031.ADFDATA.NET>
2017-06-17 16:52:53 +0530

    Stub

--- src/main/java/org/softwareag/hackthon/controller/RequestController.java
+++ src/main/java/org/softwareag/hackthon/controller/RequestController.java
@@ -3,10 +3,12 @@ package org.softwareag.hackthon.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.softwareag.hackthon.google.GoogleDistanceService;
+import org.softwareag.hackthon.response.StartResponse;
 import org.softwareag.hackthon.uber.FareEstimateService;
 import org.softwareag.hackthon.uber.ServerToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +45,33 @@ public class RequestController {
         LOG.info(googleDistanceService.getDistance(13.0000632f,80.2331078f,12.9862535f,80.2457295f).toString());
     }
     /** **/
+
+    @RequestMapping(value = "/start",method = RequestMethod.POST,
+            consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public void startRide(@RequestBody String requestbody){
+      LOG.info(requestbody);
+        StartResponse response = new StartResponse();
+        response.setStatus(200);
+        response.setTripId("12345");
+    }
+
+    @RequestMapping(value = "/detail",method = RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public void detail(@RequestBody String requestbody){
+        LOG.info(requestbody);
+        StartResponse response = new StartResponse();
+        response.setStatus(200);
+        response.setTripId("12345");
+    }
+
+    @RequestMapping(value = "/detail",method = RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public void detail(@RequestBody String requestbody){
+        LOG.info(requestbody);
+        StartResponse response = new StartResponse();
+        response.setStatus(200);
+        response.setTripId("12345");
+    }
 
 
 }
---
+++ src/main/java/org/softwareag/hackthon/response/StartResponse.java
@@ -0,0 +1,30 @@
+package org.softwareag.hackthon.response;
+
+/**
+ * Created by rvignesh on 6/17/2017.
+ */
+public class StartResponse {
+
+    private String tripId;
+
+    private int status;
+
+    public String getTripId() {
+        return tripId;
+    }
+
+    public void setTripId(String tripId) {
+        this.tripId = tripId;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+
+
+}
